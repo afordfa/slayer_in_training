58176a548d4fbdd9db409abfb2e93ca32aba951b
afordfa <anitamford@gmail.com>
2017-07-06 18:15:45 -0500

    moved files to app

--- app/server.js
+++ app/server.js
@@ -43,7 +43,7 @@ app.get("*", function(req, res) {
 // -------------------------------------------------
 
 // Listener
-db.sequelize.sync({force: false}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
