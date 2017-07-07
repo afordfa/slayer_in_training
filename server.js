f90cf732b7039bfe558a2a4ca7280a20926922ea
afordfa <anitamford@gmail.com>
2017-07-06 19:57:37 -0500

    changed force to false

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("*", function(req, res) {
 // -------------------------------------------------
 
 // Listener
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({force: false}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
